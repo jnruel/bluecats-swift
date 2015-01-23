a5febe4edddebceef88e560aa6b8279b43a15610
Joe <jruel@brella.com>
2015-01-23 16:40:10 -0600

    Initial Commit

---
+++ bluecats-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bluecats-swift
+//
+//  Created by Joe Ruel on 1/23/15.
+//  Copyright (c) 2015 Joseph Ruel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
