974787e7690d4a9099ce03f2b0a0b5c14c417884
Joe <jruel@brella.com>
2015-01-23 17:08:15 -0600

    prints beacon monitoring activity to console

---
+++ Bridging-Header.h
@@ -0,0 +1,20 @@
+//
+//  Bridging-Header.h
+//  BlueCatsTest
+//
+//  Created by Joe Ruel on 1/22/15.
+//  Copyright (c) 2015 Joseph Ruel. All rights reserved.
+//
+
+#import <BlueCatsSDK/BlueCatsSdk.h>
+#import <BlueCatsSDK/BCMicroLocationManager.h>
+#import <BlueCatsSDK/BCMicroLocation.h>
+#import <BlueCatsSDK/BCSite.h>
+#import <BlueCatsSDK/BCCategory.h>
+#import <BlueCatsSDK/BCBeacon.h>
+
+#ifndef BlueCatsTest_Bridging_Header_h
+#define BlueCatsTest_Bridging_Header_h
+
+
+#endif
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+
+
+
+pod 'BlueCatsSDK', :git => 'https://github.com/bluecats/bluecats-ios-sdk.git', :tag => '0.4.1'
+
+
+
